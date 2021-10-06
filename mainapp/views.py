eb4d4ef234b79fc884a2f51fa8365e19cbc70c7a
gamzerramp <72271736+gamzerramp@users.noreply.github.com>
2021-10-06 12:26:48 +0530

    Update views.py

--- mainapp/views.py
+++ mainapp/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from django.views.generic.edit import CreateView, UpdateView
 from django.views.generic.base import TemplateView
 
+
 from django.views.generic.list import ListView
 
 from mainapp.redis_queue import sms_queue
